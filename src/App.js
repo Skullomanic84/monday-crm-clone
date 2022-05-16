ec0ccb24c18aafc817a972ccb7ba810db583596b
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2022-05-16 15:02:55 +0200

    App.js
    page layout and ui look and feel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard"
+import TicketPage from './pages/TicketPage'
+import Nav from './components/Nav'
+
 const App = () => {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard />}/>
+          <Route path='/ticket' element={<TicketPage />}/>
+          <Route path='/ticket/:id' element={<TicketPage editMode={true}/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
